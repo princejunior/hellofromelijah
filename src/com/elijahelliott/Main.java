70293fb9e4c7abb455cde8cc96ef8a94e364b9f4
princejunior <47012681+princejunior@users.noreply.github.com>
2021-10-17 17:54:13 -0600

    Added hello

--- src/com/elijahelliott/Main.java
+++ src/com/elijahelliott/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+
         System.out.println("hello, my name is Elijah");
+        System.out.println("hello, Elijah. My name is Elijah");
+
     }
 }
